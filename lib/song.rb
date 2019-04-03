bd3ecc92d49427c732b153e8702aae090801ced9
David Stewart <davidstewart@gmail.com>
2019-04-03 01:24:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,17 @@ end
    end
      hash
  end
- 
+  def self.artist_count
+   hash = {}
+   @@artists.each do |artist|
+     if hash[artist]
+       hash[artist] += 1
+     else
+       hash[artist] = 1
+     end
+   end
+     hash
+ end
 
 
 
