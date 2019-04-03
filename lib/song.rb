a7d97846752d2ce819ae33d109bcdd8e0ea64fd8
David Stewart <davidstewart@gmail.com>
2019-04-03 00:50:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
     @artist=artist
     @genre=genre
     @@count += 1
-    ARTIST.include? artist ? false : ARTIST << artist
+    ARTIST.include? artist ? : ARTIST << artist
     binding.pry
    end
   
